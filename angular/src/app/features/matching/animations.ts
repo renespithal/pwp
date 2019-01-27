d20feb1fa2f9d87424acfaa4c7dfed145eb139b9
Rene Nespithal <rene.nespithal@gmail.com>
2019-01-27 20:06:44 +0100

    animations adjusted

--- angular/src/app/features/matching/animations.ts
+++ angular/src/app/features/matching/animations.ts
@@ -4,21 +4,29 @@ import {trigger, animate, style, transition, state} from '@angular/animations';
 export const slide = [
   trigger('slide', [
 
-  state('right', style({ transform: 'translateX(+100%)', opacity:'0'})),
-  state('left', style({ transform: 'translateX(-100%)', opacity:'0'})),
+  state('right', style({ transform: 'translateX(+300%)', opacity:'0'})),
+  state('left', style({ transform: 'translateX(-300%)', opacity:'0'})),
   state('mid', style({ opacity:'1', transform: 'none'})),
   state('hidden', style({opacity:'0', transform: 'none'})),
 
   transition('hidden => mid', [
-    animate('150ms')
+    animate('250ms')
   ]),
 
   transition('mid => left', [
-    animate('250ms')
+    animate('350ms')
   ]),
 
   transition('mid => right', [
-    animate('250ms')
-  ])
+    animate('350ms')
+  ]),
+
+  transition('right => hidden', [
+    animate('400ms')
+  ]),
+
+  transition('left => hidden', [
+    animate('400ms')
+  ]),
 
 ])];
